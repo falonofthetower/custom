3fd88671b9f9afd826b61b3bb961f68a9243c0dd
Peter Karth <falonofthetower@gmail.com>
2016-12-04 16:52:59 -0500

    didn't mean to remove THAT

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,6 @@
 require_relative 'application'
 
 # Initialize the Rails application.
-Rails.application.configure do
-  Rails.application.routes.default_url_options[:host] = ENV["HOST"]
-end
+Rails.application.initialize!
+
+Rails.application.routes.default_url_options[:host] = ENV["HOST"]
