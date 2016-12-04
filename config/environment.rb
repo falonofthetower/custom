aff037bd39bab4c82eb4010a7556da402fbc725b
Peter Karth <falonofthetower@gmail.com>
2016-12-04 18:37:02 -0500

    trying send_grid

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,14 @@ require_relative 'application'
 Rails.application.initialize!
 
 Rails.application.routes.default_url_options[:host] = ENV["HOST"]
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => ENV['HOST'],
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+ActionMailer::Base.delivery_method = :smtp
