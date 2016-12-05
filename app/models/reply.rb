8ae4c81147861727bd992bdcd4214a61e6d1cfb1
Peter Karth <falonofthetower@gmail.com>
2016-12-04 23:17:01 -0500

    Final tweaks to interface
    - ordered replies desc
    - other minor tweaks

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -3,6 +3,8 @@ class Reply < ApplicationRecord
   validates_length_of :content, minimum: 10
   belongs_to :user
   belongs_to :post
-  
+
   include ErrorHelper
+
+  scope :latest_to_oldest, -> { order('created_at desc') }
 end
